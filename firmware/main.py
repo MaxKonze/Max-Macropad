5939ee41d94a6408f566b4036addcfbe145aa120
Maximilian <konmax1801@gho365.de>
2026-04-27 20:04:37 +0200

    New firmware

--- firmware/main.py
+++ firmware/main.py
@@ -1,5 +1,6 @@
 # You import all the IOs of your board
 import board
+import time
 
 # These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
@@ -8,9 +9,9 @@ from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.modules.encoder import EncoderHandler
 from kmk.scanners import DiodeOrientation
-from kmk.extensions.RGB import RGB
-
-active_layer = 0
+from kmk.extensions.rgb import RGB
+from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.statusled import statusLED
 
 # This is the main instance of your keyboard
 keyboard = KMKKeyboard()
@@ -20,45 +21,28 @@ rotary_encoder = EncoderHandler()
 keyboard.modules.append(macros)
 keyboard.modules.append(rotary_encoder)
 
-rgb = RGB(pixel_pin=board.D6, num_pixels=2)
+#rgb = RGB(pixel_pin=board.D6, num_pixels=2,val_limit=100,val_default=50,hue_default=0,sat_default=255,rgb_order=(1,0,2))
+#eyboard.extensions.append(rgb)
 
-def change_layer_on_macro_press(keyboard, coordinate, is_pressed):
-    global active_layer
-    if is_pressed:
-        if active_layer == 0:
-            active_layer = 1
-            rgb.set_rgb_fill((255, 0, 0))  # Change to red when on layer 1    
-        else:
-            active_layer = 0
-            rgb.set_rgb_fill((0, 0, 255))  # Change to blue when on layer 0
-        keyboard.active_layers = [active_layer]
-
-LAYER_KEY = KC.NO.clone()
-LAYER_KEY.on_press_fn = (change_layer_on_macro_press)
 
 keyboard.col_pins = (board.D7, board.D8)
 keyboard.row_pins = (board.D0, board.D1, board.D2, board.D3)
 keyboard.diode_orientation = DiodeOrientation.ROW2COL
 
+keyboard.extensions.append(MediaKeys())
+
 keyboard.keymap = [
     [
-        KC.A, KC.B, 
+        KC.AUDIO_MUTE, KC.B,
         KC.C, KC.D,
-        KC.E, KC.F, 
-        KC.G, KC.LAYER_KEY,
+        KC.E, KC.F,
+        KC.G, KC.H,
     ]
-    ,
-    [
-        KC.N1, KC.N2, 
-        KC.N3, KC.N4,
-        KC.N5, KC.N6, 
-        KC.N7, KC.LAYER_KEY,
-    ],
 ]
 
 rotary_encoder.pins = ((board.D9, board.D10, None, False),)  # Define pins for one rotary encoder
 rotary_encoder.map = [
-    ((KC.VOLU, KC.VOLD),), 
+    ((KC.VOLU, KC.VOLD),),
 ]
 
 if __name__ == '__main__':
